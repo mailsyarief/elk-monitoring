28fba95a0f2fc99277c27c661e462bd451477769
mailsyarief <ismail8syarief@gmail.com>
2021-05-15 16:58:32 +0700

    change port and volume

--- initiate_replica.sh
+++ initiate_replica.sh
@@ -13,8 +13,8 @@ rs.initiate(
     _id : 'rs0',
     members: [
       { _id : 0, host : "mongodb1:27017" },
-      { _id : 1, host : "mongodb2:27018" },
-      { _id : 2, host : "mongodb3:27019" }
+      { _id : 1, host : "mongodb2:27017" },
+      { _id : 2, host : "mongodb3:27017" }
     ]
   }
 )
